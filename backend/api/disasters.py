49b174a3ee3815b6c9bbb92d657d9e69eb95b4bf
Ungarala Ramya <ungaralaramya32@gmail.com>
2026-07-05 23:23:43 +0530

    Day 3: Added disaster API with model and service layer

--- backend/api/disasters.py
+++ backend/api/disasters.py
@@ -1,20 +1,8 @@
 from fastapi import APIRouter
+from services.disaster_service import get_disasters
 
 router = APIRouter()
 
 @router.get("/disasters")
-def get_disasters():
-    return [
-        {
-            "id": 1,
-            "location": "Hyderabad",
-            "type": "Flood",
-            "severity": "High"
-        },
-        {
-            "id": 2,
-            "location": "Chennai",
-            "type": "Cyclone",
-            "severity": "Medium"
-        }
-    ]
+def disasters():
+    return get_disasters()
---
+++ backend/services/disaster_service.py
@@ -0,0 +1,30 @@
+from models.disaster import Disaster
+
+def get_disasters():
+
+    disasters = [
+
+        Disaster(
+            id=1,
+            location="Hyderabad",
+            type="Flood",
+            severity="High"
+        ),
+
+        Disaster(
+            id=2,
+            location="Chennai",
+            type="Cyclone",
+            severity="Medium"
+        ),
+
+        Disaster(
+            id=3,
+            location="Delhi",
+            type="Earthquake",
+            severity="Low"
+        )
+
+    ]
+
+    return disasters
